67f51baf8da3e7955c318183adf82190bdb0d004
Rebecca <becca.siegal@gmail.com>
2020-03-21 11:16:01 -0400

    updated addFolder and addNotes

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -11,7 +11,7 @@ export default class AddNote extends Component {
     return (
       <section className='AddNote'>
         <h2>Create a note</h2>
-        <NotefulForm>
+        <NotefulForm onSubmit={this.handleNoteSubmit}>
           <div className='field'>
             <label htmlFor='note-name-input'>
               Name
